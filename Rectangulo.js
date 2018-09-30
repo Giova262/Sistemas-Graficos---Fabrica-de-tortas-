bce8f46bd2d064cd49a8de9208e6924dd89d0fb0
MatiasAnas <matias.anas@gmail.com>
2018-09-30 19:55:22 -0300

    Cree la clase Rectangulo.

---
+++ Rectangulo.js
@@ -0,0 +1,110 @@
+class Rectangulo {
+	constructor(gl, ancho, alto, profundidad, color) {
+		this.ancho = ancho;
+		this.alto = alto;
+		this.profundidad = profundidad;
+		this.color = color;
+		
+		this.TRIANGULOS = 24;
+
+		// Buffers.
+		this.position_buffer = [];
+		this.index_buffer = [];
+        this.color_buffer = [];
+        
+        this.crearIndexBuffer();
+        this.crearPositionBuffer();
+        this.crearColorBuffer();
+    	this.setupBuffers();
+	}
+	
+	crearIndexBuffer() {
+		for(var i = 0; i < this.TRIANGULOS ; i += 4){
+			this.index_buffer.push(...[i, (i + 1), (i + 2), i, (i + 2), (i + 3)]);
+		}
+	}
+	
+	crearPositionBuffer() {
+	
+		// Sistema de coordenadas en el centro de la cara inferior. Centrado en z = 0.
+		var x = this.ancho / 2;
+		var y = this.alto / 2;
+		var z = this.profundidad;
+	
+		this.position_buffer = [
+		
+		// Cara Frontal.
+			-x, 0,  y,
+			 x, 0,  y,
+			 x, z,  y,
+			-x, z,  y,
+					
+		// Cara Posterior.
+			-x, 0, -y,
+			-x, z, -y,
+			 x, z, -y,
+			 x, 0, -y,
+					
+		// Cara Superior.
+			-x, z, -y,
+			-x, z,  y,
+			 x, z,  y,
+			 x, z, -y,
+					
+		// Cara Inferior.
+			-x, 0, -y,
+			 x, 0, -y,
+			 x, 0,  y,
+			-x, 0,  y,
+					
+		// Cara Lateral Derecha.
+			 x, 0, -y,
+			 x, z, -y,
+			 x, z,  y,
+			 x, 0,  y,
+					
+		// Cara Lateral Izquierda.
+			-x, 0, -y,
+			-x, 0,  y,
+			-x, z,  y,
+			-x, z, -y
+			
+		];
+	}
+	
+	crearColorBuffer() {
+		for (var i = 0; i < this.TRIANGULOS * 3; i += 3){
+			this.color_buffer.push(... this.color);
+		}
+	}
+	
+	setupBuffers() {
+		this.webgl_position_buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.position_buffer), gl.STATIC_DRAW);
+
+		this.webgl_color_buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+		gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.color_buffer), gl.STATIC_DRAW);   
+
+		this.webgl_index_buffer = gl.createBuffer();
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+		gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
+   	}
+
+	dibujar() {
+		var vertexPositionAttribute = gl.getAttribLocation(glProgram, "aVertexPosition");
+		gl.enableVertexAttribArray(vertexPositionAttribute);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+		gl.vertexAttribPointer(vertexPositionAttribute, 3, gl.FLOAT, false, 0, 0);
+
+		var vertexColorAttribute = gl.getAttribLocation(glProgram, "aVertexColor");
+		gl.enableVertexAttribArray(vertexColorAttribute);
+		gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+		gl.vertexAttribPointer(vertexColorAttribute, 3, gl.FLOAT, false, 0, 0);
+
+		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+
+		gl.drawElements(gl.TRIANGLE_STRIP, this.index_buffer.length, gl.UNSIGNED_SHORT, 0);
+	}
+}
