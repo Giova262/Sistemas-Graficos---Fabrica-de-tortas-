f6e5e9c24674744398a1a902a6378dfaa0dbb2e2
MatiasAnas <matias.anas@gmail.com>
2018-10-07 16:30:14 -0300

    Cree la clase Copito.js y CurvaBezier.js.

---
+++ Copito.js
@@ -0,0 +1,46 @@
+/*
+	Uso:
+	
+	// Creo la geometria.
+	var copitoGeometria = new Copito(gl);
+	
+	//Creo un objeto 3D con geometria copito (Consultar clase Objeto3D).
+    copito = new Objeto3D(gl, copitoGeometria); 
+*/
+
+
+class Copito {
+	constructor(gl) {
+		var puntos_de_control = [0.0,	0.0,	1.0,
+								 0.0,	0.0,	1.0,
+								 0.0,	0.0,	1.0,
+								 0.0,	0.0,	1.0,
+								 
+							     0.025,	0.0,	1.0,
+								 0.025,	0.0,	0.7,
+								 0.32,	0.0,	0.6,
+								 0.32,	0.0,	0.2,
+								 
+								 0.32,	0.0,	0.2,
+								 0.32,	0.0,	0.1,
+								 0.5,	0.0,	0.1,
+								 0.5,	0.0,	0.0,
+								 
+								 0.0,	0.0,	0.0,
+								 0.0,	0.0,	0.0,
+								 0.0,	0.0,	0.0,
+								 0.0,	0.0,	0.0,
+		];
+		
+		var puntos_detalle_curva = 40;
+		var puntos_detalle_revolucion = 40;
+		
+		var curva = new CurvaBezier(puntos_de_control, puntos_detalle_curva);
+		
+		this.superficie = new SuperficieDeRevolucion(gl, curva.getPosiciones(), puntos_detalle_revolucion);
+	}
+	
+	dibujar() {
+		this.superficie.dibujar();
+	}
+}
